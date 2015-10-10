879c7afd884129cf43e5a53264a34bc842d2b227
Ben Centra <bencentra@users.noreply.github.com>
2015-10-09 21:39:27 -0400

    Part 2

--- src/main.c
+++ src/main.c
@@ -1,28 +1,43 @@
 #include <pebble.h>
   
 /*
-*  https://developer.getpebble.com/tutorials/watchface-tutorial/part1
+*  Part 1: https://developer.getpebble.com/tutorials/watchface-tutorial/part1
+*  Part 2: https://developer.getpebble.com/tutorials/watchface-tutorial/part2/
 */
   
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
+static BitmapLayer *s_background_layer;
+static GBitmap *s_background_bitmap;
 
 static void main_window_load(Window *window) {
+  // Create GFont
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+  // Create GBitmap, then set to created BitmapLayer
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
   // Create the TextLayer
-  s_time_layer = text_layer_create(GRect(0,55,144,50));
+  s_time_layer = text_layer_create(GRect(5,52,139,50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
-  // text_layer_set_text(s_time_layer, "00:00");
   // Improve the layout to be more like a watchface
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
+  // Destroy the GBitmap and BitmapLayer
+  gbitmap_destroy(s_background_bitmap);
+  bitmap_layer_destroy(s_background_layer);
   // Destroy the TextLayer
   text_layer_destroy(s_time_layer);
+  // Unload the GFont
+  fonts_unload_custom_font(s_time_font);
 }
 
 static void update_time() {
@@ -66,7 +81,6 @@ static void deinit() {
   // Destroy the Window
   window_destroy(s_main_window);
 }
-
 
 int main(void) {
   init();
